a7172fd0a90dc59ac27b1ebda4f33acc1164aca9
ckrb63 <cksr1@naver.com>
2022-08-28 16:09:22 +0900

    Add record header

---
+++ DelgoReward/src/common/components/RecordHeader.tsx
@@ -0,0 +1,30 @@
+import classNames from "classnames";
+import React, { useState } from "react";
+import "./RecordHeader.scss";
+
+function RecordHeader() {
+  const [selected, setSeleceted] = useState({
+    photo: false,
+    calendar: true,
+    map: false,
+  });
+
+  const clickHandler = (e: any) => {
+    const {id} = e.target;
+    setSeleceted((prev)=>{
+      const temp = {
+        photo: false,
+        calendar: false,
+        map : false,
+      }
+      return {...temp, [id]: true}
+    })
+  }
+  return <div className="recordHeader">
+    <div aria-hidden="true" id="photo" className={classNames("recordHeader-item", { select: selected.photo })} onClick={clickHandler}>사진</div>
+    <div aria-hidden="true" id="calendar" className={classNames("recordHeader-item", { select: selected.calendar })} onClick={clickHandler}>캘린더</div>
+    <div aria-hidden="true" id="map" className={classNames("recordHeader-item", { select: selected.map })} onClick={clickHandler}>지도</div>
+  </div>
+}
+
+export default RecordHeader;
--- DelgoReward/src/pages/CalendarPage.tsx
+++ DelgoReward/src/pages/CalendarPage.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import RecordHeader from "../common/components/RecordHeader";
 import Calender from "../common/utils/Calender";
 
-function CalendarPage(){
-    return <Calender/>
+function CalendarPage() {
+    return <>
+      <RecordHeader/>
+      <Calender />
+    </>
 };
 
 export default CalendarPage;
