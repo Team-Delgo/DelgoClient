6f14e3f3763468cbb91333004ecf4163af911568
jungseokwoo <wjdtjrdn6@gmail.com>
2022-09-04 18:21:23 +0900

    Add: bottom camera navigate

--- src/common/components/FooterNavigation.tsx
+++ src/common/components/FooterNavigation.tsx
@@ -1,23 +1,33 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
+import { CAMERA_PATH } from '../constants/path.const'
 import "./FooterNavigation.scss";
 import Home from "../icons/home.svg";
 import Plus from "../icons/plus.svg";
 import DogFoot from "../icons/dog-foot.svg";
 
 function FooterNavigation() {
-  return <div className="footer">
-    <div className="footer-side">
-      <img src={Home} alt="home-button" />
-    </div>
-    <div className="footer-center">
-      <div className="footer-center-circle">
-        <img src={Plus} alt="plus-button" />
+  const navigate = useNavigate();
+
+  const moveToCameraPage = () => {
+    navigate(CAMERA_PATH.MAIN);
+  };
+
+  return (
+    <div className="footer">
+      <div className="footer-side">
+        <img src={Home} alt="home-button" />
+      </div>
+      <div className="footer-center" aria-hidden="true" onClick={moveToCameraPage}>
+        <div className="footer-center-circle">
+          <img src={Plus} alt="plus-button" />
+        </div>
+      </div>
+      <div className="footer-side">
+        <img src={DogFoot} alt="record-button" />
       </div>
     </div>
-    <div className="footer-side">
-      <img src={DogFoot} alt="record-button" />
-    </div>
-  </div>
+  );
 }
 
 export default FooterNavigation;
--- src/pages/camera/capture/CapturePage.tsx
+++ src/pages/camera/capture/CapturePage.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useCallback, useRef, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { ReactComponent as PrevArrowWhite } from '../../../common/icons/prev-arrow-white.svg';
+import PrevArrowWhite from '../../../common/icons/prev-arrow-white.svg';
 import { CAMERA_PATH } from '../../../common/constants/path.const'
 import './CapturePage.scss';
 
@@ -23,35 +23,41 @@ function CapturePage() {
   return (
     <>
       <img className="captured-img" src={state.img} height={220} width={280} alt="caputeImg" />
-      <PrevArrowWhite className="capture-page-prev-arrow" onClick={moveToPreviousPage} />
+      <img
+        src={PrevArrowWhite}
+        className="capture-page-prev-arrow"
+        alt="capture-page-prev-arrow"
+        aria-hidden="true"
+        onClick={moveToPreviousPage}
+      />
       <div className="capture-img-record">
         <header className="capture-img-record-header">어떤 기록을 남기시겠어요?</header>
         <body className="capture-img-record-body">
-          <div className="capture-img-record-body-walk" aria-hidden="true" onClick={moveToCategoryPage("산책")}>
+          <div className="capture-img-record-body-walk" aria-hidden="true" onClick={moveToCategoryPage('산책')}>
             <div className="capture-img-record-body-walk-label">산책</div>
             <div className="capture-img-record-body-walk-count">0</div>
           </div>
-          <div className="capture-img-record-body-cafe" aria-hidden="true" onClick={moveToCategoryPage("카페")}>
+          <div className="capture-img-record-body-cafe" aria-hidden="true" onClick={moveToCategoryPage('카페')}>
             <div className="capture-img-record-body-cafe-label">카페</div>
             <div className="capture-img-record-body-cafe-count">0</div>
           </div>
-          <div className="capture-img-record-body-restaurant" aria-hidden="true" onClick={moveToCategoryPage("식당")}>
+          <div className="capture-img-record-body-restaurant" aria-hidden="true" onClick={moveToCategoryPage('식당')}>
             <div className="capture-img-record-body-restaurant-label">식당</div>
             <div className="capture-img-record-body-restaurant-count">0</div>
           </div>
-          <div className="capture-img-record-body-hospital" aria-hidden="true" onClick={moveToCategoryPage("병원")}>
+          <div className="capture-img-record-body-hospital" aria-hidden="true" onClick={moveToCategoryPage('병원')}>
             <div className="capture-img-record-body-hospital-label">병원</div>
             <div className="capture-img-record-body-hospital-count">0</div>
           </div>
-          <div className="capture-img-record-body-beauty" aria-hidden="true" onClick={moveToCategoryPage("미용")}>
+          <div className="capture-img-record-body-beauty" aria-hidden="true" onClick={moveToCategoryPage('미용')}>
             <div className="capture-img-record-body-beauty-label">미용</div>
             <div className="capture-img-record-body-beauty-count">0</div>
           </div>
-          <div className="capture-img-record-body-bath" aria-hidden="true" onClick={moveToCategoryPage("목욕")}>
+          <div className="capture-img-record-body-bath" aria-hidden="true" onClick={moveToCategoryPage('목욕')}>
             <div className="capture-img-record-body-bath-label">목욕</div>
             <div className="capture-img-record-body-bath-count">0</div>
           </div>
-          <div className="capture-img-record-body-etc" aria-hidden="true" onClick={moveToCategoryPage("기타")}>
+          <div className="capture-img-record-body-etc" aria-hidden="true" onClick={moveToCategoryPage('기타')}>
             <div className="capture-img-record-body-etc-label">기타</div>
             <div className="capture-img-record-body-etc-count">0</div>
           </div>
