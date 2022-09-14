f912164f96fea977b52b1c56a1997b62e36002d0
ckrb63 <cksr1@naver.com>
2022-09-14 15:21:41 +0900

    ADD mungple toggle

--- src/pages/map/MapPage.tsx
+++ src/pages/map/MapPage.tsx
@@ -1,31 +1,37 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import FooterNavigation from "../../common/components/FooterNavigation";
 import RecordHeader from "../../common/components/RecordHeader";
 import "./MapPage.scss";
 import park from "./park.jpg";
 import { dummyData } from "./dummypin";
+import { dummyMungple } from "./dummymungple";
 
 function MapPage() {
   const mapElement = useRef(null);
   const navigate = useNavigate();
+  const [mungple, setMungple] = useState('ON');
+  const [globarMap, setGlobarMap] = useState<naver.maps.Map>();
+  const [currentLocation, setCurrentLocation] = useState({lat:dummyData[0].lat, lng:dummyData[0].lng, zoom:17});
 
   const pinButtonHandler = () => {
     console.log(1);
   };
 
+  let map: naver.maps.Map;
+
   useEffect(() => {
     if (!mapElement.current || !naver) return;
-    const location = new window.naver.maps.LatLng(dummyData[0].lat, dummyData[0].lng);
+    const location = new window.naver.maps.LatLng(currentLocation.lat, currentLocation.lng);
     const mapOptions: naver.maps.MapOptions = {
       center: location,
-      zoom: 17,
+      zoom: currentLocation.zoom,
       zoomControl: true,
       // zoomControlOptions: {
       //   position: window.naver.maps.Position.TOP_RIGHT,
       // },
     };
-    const map = new naver.maps.Map(mapElement.current, mapOptions);
+    map = new naver.maps.Map(mapElement.current, mapOptions);
 
     dummyData.forEach((data) => {
       const markerOptions = {
@@ -44,16 +50,46 @@ function MapPage() {
       const marker = new naver.maps.Marker(markerOptions);
       marker.addListener('click', () => { navigate(`/post/?userId=${1}&postId=${data.id}`) });
     })
-  }, []);
+    if(mungple === 'ON'){
+    dummyMungple.forEach((data) => {
+      const markerOptions = {
+        position: new window.naver.maps.LatLng(data.lat, data.lng),
+        map,
+        icon: {
+          content: [
+            `<div class="mungple" style="z-index:${data.id}"/>`,
+          ].join(''),
+          size: new naver.maps.Size(13, 13),
+          origin: new naver.maps.Point(0, 0),
+        }
+      };
+      const marker = new naver.maps.Marker(markerOptions);
+    })
+  }
+    setGlobarMap(map);
+  }, [mungple]);
 
   const mapStyle = {
     width: '100vw',
     height: '80vh',
   }
 
+  const mungpleButtonHandler = () => {
+    const location = globarMap?.getCenter();
+    const zoom = globarMap!.getZoom();
+    setCurrentLocation({lat:location!.y, lng:location!.x, zoom})
+    if (mungple === 'ON')
+      setMungple('OFF');
+    else {
+      setMungple('ON');
+    }
+  };
+
   return <div>
     <RecordHeader />
-    <div ref={mapElement} style={mapStyle} />
+    <div ref={mapElement} style={mapStyle} >
+      <div aria-hidden="true" onClick={mungpleButtonHandler} className="mungplace-toggle" >{`멍플 ${mungple}`}</div>
+    </div>
     <FooterNavigation />
   </div>
 }
---
+++ src/pages/map/dummymungple.ts
@@ -0,0 +1,19 @@
+const dummyMungple = [
+  {
+    lat:37.5067,
+    lng:127.1061,
+    id:5,
+  },
+  {
+    lat:37.5100,
+    lng:127.1090,
+    id:3,
+  },
+  {
+    lat:37.5675,
+    lng:127.0022,
+    id:1,
+  }
+]
+
+export {dummyMungple};
