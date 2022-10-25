802b9c8adfd15820809abaec118a22feccc0cf01
정석우 <ingal777@naver.com>
2022-10-26 01:49:07 +0900

    Modify: posts page

--- src/pages/post/PostsPage.tsx
+++ src/pages/post/PostsPage.tsx
@@ -49,16 +49,26 @@ function PostsPage() {
         <>
           <header className="post-img-result-header">
             <div className="post-img-result-header-date">
-              {post.date} {post.weekDay} {post.time}
+              {post.date} {post.weekDay} &nbsp; {post.time}
             </div>
           </header>
           <main className="post-img-result-main">
             <img src={post.img} width={window.innerWidth} height={window.innerWidth} alt="postImg" />
-            <header className="post-img-result-main-header">
-              <div className="post-img-result-main-header-title">{post.title}</div>
-              <div className={`post-img-result-main-header-${post.category}`}>{post.categoryKo}</div>
+            <header className="post-img-result-main-profile">
+              <img
+                className="post-img-result-main-profile-img"
+                src={`${process.env.PUBLIC_URL}/assets/dog-img.png`}
+                alt="copy url"
+              />
+              <div className="post-img-result-main-profile-second">
+                <div className="post-img-result-main-profile-second-address">서울시 송파구</div>
+                <div className="post-img-result-main-profile-second-name">몽자</div>
+              </div>
             </header>
-            <body className="post-img-result-main-content">{post.content}</body>
+            <body className="post-img-result-main-body">
+              <div className="post-img-result-main-body-title">{post.title}</div>
+              <div className="post-img-result-main-body-content">{post.content}</div>
+            </body>
           </main>
         </>
       ))}
