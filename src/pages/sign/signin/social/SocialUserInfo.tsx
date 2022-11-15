4af5cafb1500994af73c5aaa2aee72e864b19b00
ckrb63 <cksr1@naver.com>
2022-11-15 17:43:45 +0900

    FIX : SOCIAL USER INFO

--- src/pages/sign/signin/social/SocialUserInfo.tsx
+++ src/pages/sign/signin/social/SocialUserInfo.tsx
@@ -4,7 +4,7 @@ import { useDispatch } from 'react-redux';
 import { AxiosResponse } from 'axios';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { ReactComponent as Arrow } from '../../../../common/icons/left-arrow.svg';
-// import './UserInfo.scss';
+import '../../signup/userinfo/UserInfo.scss';
 import { SIGN_UP_PATH } from '../../../../common/constants/path.const';
 import { checkEmail, checkPassword, checkPasswordConfirm, checkNickname } from '../../validcheck';
 import { emailCheck, nicknameCheck } from '../../../../common/api/signup';
@@ -14,20 +14,16 @@ import RegionSelector from '../../signup/userinfo/RegionSelector';
 
 interface LocationState {
   phone: string;
+  isSocial: string;
+  email: string;
 }
 
 interface Input {
-  email: string;
-  password: string;
-  confirm: string;
   nickname: string;
   region: string;
 }
 
 enum Id {
-  EMAIL = 'email',
-  PASSWORD = 'password',
-  CONFIRM = 'confirm',
   NICKNAME = 'nickname',
   REGION = 'region',
 }
@@ -45,11 +41,11 @@ function SocialUserInfo() {
   const navigation = useNavigate();
   const dispatch = useDispatch();
   const state = useLocation().state as LocationState;
-  const { phone } = state;
+  const { phone, isSocial, email } = state;
   const [nextPage, setNextPage] = useState(false);
-  const [enteredInput, setEnteredInput] = useState({ email: '', password: '', confirm: '', nickname: '', region: '' });
-  const [validInput, setValidInput] = useState({ email: '', password: '', confirm: '', nickname: '', region: '' });
-  const [feedback, setFeedback] = useState({ email: '', password: '', confirm: '', nickname: '', region: '' });
+  const [enteredInput, setEnteredInput] = useState({ nickname: '', region: '' });
+  const [validInput, setValidInput] = useState({ nickname: '', region: '' });
+  const [feedback, setFeedback] = useState({ nickname: '', region: '' });
   const [confirmIsTouched, setConfirmIsTouched] = useState(false);
   const [emailDuplicated, setEmailDuplicated] = useState(true);
   const [emailDupCheckFail, setEmailDupCheckFail] = useState(false);
@@ -60,97 +56,14 @@ function SocialUserInfo() {
   const [region, setRegion] = useState<Region>();
   const emailRef = useRef<HTMLInputElement>(null);
   const nicknameRef = useRef<HTMLInputElement>(null);
-  const firstPageIsValid =
-    validInput.email.length && validInput.password.length && validInput.confirm.length && !emailDuplicated;
+  const getRegionData = async () => {
+    const response = await GetRegion();
+    setRegionList(response);
+  };
 
-    const getRegionData = async () => {
-      const response = await GetRegion();
-      setRegionList(response);
-    };
-
-  useEffect(()=>{
+  useEffect(() => {
     getRegionData();
-  },[]);
-
-  const emailValidCheck = (value: string) => {
-    const response = checkEmail(value);
-
-    if (!response.isValid) {
-      setValidInput((prev: Input) => {
-        return { ...prev, email: '' };
-      });
-    } else {
-      setValidInput((prev: Input) => {
-        return { ...prev, email: value };
-      });
-    }
-
-    setFeedback((prev: Input) => {
-      return { ...prev, email: response.message };
-    });
-  };
-
-  const passwordValidCheck = (value: string) => {
-    const response = checkPassword(value);
-
-    if (!response.isValid) {
-      setValidInput((prev: Input) => {
-        return { ...prev, password: '' };
-      });
-    } else {
-      setValidInput((prev: Input) => {
-        return { ...prev, password: value };
-      });
-    }
-
-    if (confirmIsTouched && !response.isValid) {
-      const { confirm } = enteredInput;
-      const check = checkPasswordConfirm(value, confirm);
-
-      setFeedback((prev: Input) => {
-        return { ...prev, confirm: check.message };
-      });
-    }
-
-    setFeedback((prev: Input) => {
-      return { ...prev, password: response.message };
-    });
-    if (enteredInput.confirm.length) {
-      const { confirm } = enteredInput;
-      const check = checkPasswordConfirm(value, confirm);
-      setFeedback((prev: Input) => {
-        return { ...prev, confirm: check.message };
-      });
-      if (!check.isValid) {
-        setValidInput((prev: Input) => {
-          return { ...prev, confirm: '' };
-        });
-      } else {
-        setValidInput((prev: Input) => {
-          return { ...prev, confirm: value };
-        });
-      }
-    }
-  };
-
-  const passwordConfirmValidCheck = (value: string) => {
-    const { password } = enteredInput;
-    const response = checkPasswordConfirm(password, value);
-
-    if (!response.isValid) {
-      setValidInput((prev: Input) => {
-        return { ...prev, confirm: '' };
-      });
-    } else {
-      setValidInput((prev: Input) => {
-        return { ...prev, confirm: value };
-      });
-    }
-    setFeedback((prev: Input) => {
-      return { ...prev, confirm: response.message };
-    });
-    setConfirmIsTouched(true);
-  };
+  }, []);
 
   const nicknameValidCheck = (value: string) => {
     const response = checkNickname(value);
@@ -175,13 +88,12 @@ function SocialUserInfo() {
     setTimeout(() => {
       navigation(SIGN_UP_PATH.USER_PET_INFO, {
         state: {
-          email: enteredInput.email,
-          password: enteredInput.password,
           nickname: enteredInput.nickname,
           phone,
-          isSocial: false,
+          email,
+          isSocial,
           geoCode: region!.geoCode,
-          pGeoCode: region!.pGeoCode
+          pGeoCode: region!.pGeoCode,
         },
       });
     }, 200);
@@ -194,42 +106,12 @@ function SocialUserInfo() {
       return { ...prev, [id]: value };
     });
 
-    if (id === Id.EMAIL) {
-      emailValidCheck(value);
-      setEmailDuplicated(true);
-      setEmailDupCheckFail(false);
-    } else if (id === Id.PASSWORD) {
-      passwordValidCheck(value);
-    } else if (id === Id.CONFIRM) {
-      passwordConfirmValidCheck(value);
-    } else if (id === Id.REGION) {
+    if (id === Id.REGION) {
       // console.log(id);
     } else {
       nicknameValidCheck(value);
       setNicknameDuplicated(true);
       setNicknameDupCheckFail(false);
-    }
-  };
-
-  const emailDupCheck = async () => {
-    if (validInput.email) {
-      emailCheck(
-        enteredInput.email,
-        (response: AxiosResponse) => {
-          const { code } = response.data;
-          if (code === 200) {
-            setEmailDuplicated(false);
-            setEmailDupCheckFail(false);
-          } else {
-            setEmailDuplicated(true);
-            setEmailDupCheckFail(true);
-            if (emailRef.current) {
-              emailRef.current.focus();
-            }
-          }
-        },
-        dispatch,
-      );
     }
   };
 
@@ -263,7 +145,6 @@ function SocialUserInfo() {
     setRegion(region);
   };
 
-
   const closeModal = () => {
     setModalActive(false);
   };
@@ -286,140 +167,77 @@ function SocialUserInfo() {
         <Arrow />
       </div>
       <header className="login-header">필수 정보 입력</header>
-      {!nextPage && (
-        <>
-          <span className="login-span">이메일</span>
-          <div className="login-input-box">
-            <input
-              className={classNames('login-input email', { invalid: feedback.email || emailDupCheckFail })}
-              placeholder="이메일"
-              id={Id.EMAIL}
-              value={enteredInput.email}
-              autoComplete="false"
-              onChange={inputChangeHandler}
-              ref={emailRef}
-            />
-            {!emailDuplicated && validInput.email && (
-              <span className={classNames('login-input-clear', { checked: !emailDuplicated })}>
-                <img src={Check} alt="check" />
-              </span>
-            )}
-            <p className={classNames('input-feedback', { fine: !emailDuplicated && validInput.email.length })}>
-              {emailDupCheckFail ? '이미 사용중인 이메일입니다.' : feedback.email}
-            </p>
 
-            <span aria-hidden="true" className="input-email-check" onClick={emailDupCheck}>
-              중복확인
-            </span>
+        {modalActive && (
+          <div>
+            <div
+              aria-hidden="true"
+              className="backdrop"
+              onClick={() => {
+                setModalActive(false);
+              }}
+            />
+            <div className="modal">
+              <RegionSelector
+                list={regionList!}
+                close={closeModal}
+                change={regionChangeHandler}
+                rIndex={region?.indexRegion}
+                pIndex={region?.indexPlace}
+              />
+            </div>
           </div>
-          <span className="login-span">비밀번호</span>
+        )}
+        <span className="login-span">닉네임</span>
+        <div className="login-input-box">
           <input
-            className={classNames('login-input password', { invalid: feedback.password.length })}
-            placeholder="비밀번호 최소 8자이상 (문자, 숫자 조합)"
-            type="password"
-            value={enteredInput.password}
-            autoComplete="false"
-            id={Id.PASSWORD}
+            className={classNames('login-input email', {
+              invalid: feedback.nickname || nicknameDupCheckFail,
+            })}
+            placeholder="닉네임(공백, 특수문자 제외)"
+            id={Id.NICKNAME}
+            value={enteredInput.nickname}
+            autoComplete="off"
+            onChange={inputChangeHandler}
+            ref={nicknameRef}
+          />
+          {!nicknameDuplicated && validInput.nickname && (
+            <span className={classNames('login-input-clear', { checked: !nicknameDuplicated })}>
+              <img src={Check} alt="check" />
+            </span>
+          )}
+          <p className={classNames('input-feedback', { fine: !nicknameDuplicated && validInput.nickname.length })}>
+            {nicknameDupCheckFail ? '이미 사용중인 닉네임입니다.' : feedback.nickname}
+          </p>
+          <span aria-hidden="true" className="input-email-check" onClick={nicknameDupCheck}>
+            중복확인
+          </span>
+        </div>
+        <span className="login-span">지역</span>
+        <div className="login-input-wrapper">
+          <input
+            className={classNames('login-input input-location')}
+            placeholder="지역"
+            value={enteredInput.region}
+            id={Id.REGION}
+            onClick={() => {
+              setModalActive(true);
+            }}
+            onFocus={() => {
+              setModalActive(true);
+            }}
+            required
             onChange={inputChangeHandler}
           />
-          <div className="login-input-box">
-            <input
-              className={classNames('login-input bitmargin password', { invalid: feedback.confirm.length })}
-              placeholder="비밀번호 확인"
-              type="password"
-              value={enteredInput.confirm}
-              autoComplete="off"
-              id={Id.CONFIRM}
-              onChange={inputChangeHandler}
-            />
-            <p className="input-feedback">{feedback.password.length ? feedback.password : feedback.confirm}</p>
-          </div>
-          <button
-            type="button"
-            disabled={!firstPageIsValid}
-            className={classNames('login-button', { active: firstPageIsValid })}
-            onClick={() => {
-              setNextPage(true);
-            }}
-          >
-            다음
-          </button>
-        </>
-      )}
-      {nextPage && (
-        <>
-          {modalActive && (
-            <div>
-              <div
-                aria-hidden="true"
-                className="backdrop"
-                onClick={() => {
-                  setModalActive(false);
-                }}
-              />
-              <div className="modal">
-                <RegionSelector
-                  list={regionList!}
-                  close={closeModal}
-                  change={regionChangeHandler}
-                  rIndex={region?.indexRegion}
-                  pIndex={region?.indexPlace}
-                />
-              </div>
-            </div>
-          )}
-          <span className="login-span">닉네임</span>
-          <div className="login-input-box">
-            <input
-              className={classNames('login-input email', {
-                invalid: feedback.nickname || nicknameDupCheckFail,
-              })}
-              placeholder="닉네임(공백, 특수문자 제외)"
-              id={Id.NICKNAME}
-              value={enteredInput.nickname}
-              autoComplete="off"
-              onChange={inputChangeHandler}
-              ref={nicknameRef}
-            />
-            {!nicknameDuplicated && validInput.nickname && (
-              <span className={classNames('login-input-clear', { checked: !nicknameDuplicated })}>
-                <img src={Check} alt="check" />
-              </span>
-            )}
-            <p className={classNames('input-feedback', { fine: !nicknameDuplicated && validInput.nickname.length })}>
-              {nicknameDupCheckFail ? '이미 사용중인 닉네임입니다.' : feedback.nickname}
-            </p>
-            <span aria-hidden="true" className="input-email-check" onClick={nicknameDupCheck}>
-              중복확인
-            </span>
-          </div>
-          <span className="login-span">지역</span>
-          <div className="login-input-wrapper">
-            <input
-              className={classNames('login-input input-location')}
-              placeholder="지역"
-              value={enteredInput.region}
-              id={Id.REGION}
-              onClick={() => {
-                setModalActive(true);
-              }}
-              onFocus={() => {
-                setModalActive(true);
-              }}
-              required
-              onChange={inputChangeHandler}
-            />
-          </div>
-          <button
-            type="button"
-            disabled={!validInput.nickname.length || nicknameDuplicated}
-            className={classNames('login-button', { active: validInput.nickname.length && !nicknameDuplicated })}
-            onClick={submitHandler}
-          >
-            다음
-          </button>
-        </>
-      )}
+        </div>
+        <button
+          type="button"
+          disabled={!validInput.nickname.length || nicknameDuplicated}
+          className={classNames('login-button', { active: validInput.nickname.length && !nicknameDuplicated })}
+          onClick={submitHandler}
+        >
+          다음
+        </button>
     </div>
   );
 }
